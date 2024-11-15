224396000cec8ff2784dce7416c0b1fd9b6eb2a8
AnantHansras <ananthansras.16@gmail.com>
2024-11-15 11:07:01 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+        Blitz
+    </div>
+  )
+}
+export default App
+/*
+Home 
+About Us
+Theme
+events
+pronites
+sponsers
+our team
+campus embassador
+Register(user id)
+
+Pages
+1.) Registration page(backend)
+2.) AboutUs Page 
+3.) Event Page
+4.) Sponser Page
+5.) Campus Embassador
+6.) Merchandise page(optional)
+
+Scroll over:
+Home 
+aboutus 
+theme
+
+framermotion and 3js , explore these libraries
+*/
---
+++ src/Pages/CampusEmbassador.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CampusEmbassador = () => {
+  return (
+    <div>CampusEmbassador</div>
+  )
+}
+
+export default CampusEmbassador
