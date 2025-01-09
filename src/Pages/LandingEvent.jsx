2b0368d5566084e63bec1a7dfd2eaf23ddc47d14
aabhinavvvvv <abhinavguptaxiia1443@gmail.com>
2025-01-09 22:45:20 +0530

    random

---
+++ src/Pages/LandingEvent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LandingEvent = () => {
+  return (
+    <div>
+        <h1>Landing Event</h1>
+        <p>Welcome to our event page!</p>
+        <p>This page will display information about our upcoming event.</p>
+        <p>Please check back here for updates.</p>
+      
+    </div>
+  )
+}
+
+export default LandingEvent
