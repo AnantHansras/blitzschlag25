40bd7cadefec61bf0549a11f9bf1cd9b5485474e
AnantHansras <ananthansras.16@gmail.com>
2025-01-10 21:26:51 +0530

    images added

--- src/Pages/Event.jsx
+++ src/Pages/Event.jsx
@@ -38,8 +38,11 @@ const Events = () => {
 
   // Handle tab change
   const handleTabChange = (tab) => {
-    setActiveTab(tab);
+    if(tab == "flagship"){
+      setActiveTab(tab);
     setShowEvents(true); // Show events when a category is selected
+    }
+    
   };
 
   // Handle back to categories
@@ -282,10 +285,10 @@ const Events = () => {
                     className="w-full mt-4"
                   >
                     <img
-                      src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      src={event.imgUrl}
                       height="1000"
                       width="1000"
-                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                      className="h-60 w-full bg-cover  aspect-auto rounded-xl group-hover/card:shadow-xl"
                       alt="thumbnail"
                     />
                   </CardItem>
